02373cc3ea98820fb3282c6fc377eaafa7d2be14
Adithya B <108603566+adithya1306@users.noreply.github.com>
2023-12-20 17:45:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://adibala1306:AdiHari9@cluster0.mcf2ptl.mongodb.net/?retryWrites=true&w=majority"
+  mongoURI: "mongodb+srv://<username>:<pasword>@cluster0.mcf2ptl.mongodb.net/?retryWrites=true&w=majority"
 };
