78669f27c6ac6babc7157d5ca7abeafe80142a51
Adithya B <108603566+adithya1306@users.noreply.github.com>
2023-12-20 17:30:57 +0530

    Add files via upload

---
+++ routes/tourRoutes.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+const express = require('express')
+const router = express.Router();
+const tourController = require('./../controllers/tourController');
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours,tourController.getAllTours);
+
+router 
+  .route('/tour-stats')
+  .get(tourController.getTourStats);
+
+router
+  .route('/monthly-plan/:year')
+  .get(tourController.getMonthlyPlan);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router  
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTours)
+  .delete(tourController.deleteTour);
+
+
+module.exports = router;
